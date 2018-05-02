e4321fcdba44ed92a8d933ffdb1d3b5181de9f95
carlo <wudaoluo@163.com>
2018-05-02 22:18:24 +0800

    增加 glog 模块

--- test/b.go
+++ test/b.go
@@ -1,27 +1,28 @@
 package main
 
 import (
-	"sync/atomic"
+	//"sync/atomic"
 	"fmt"
 	"time"
+	"sync"
 )
 
 
-var connCount int32
+var connCount1 int32
+var mu sync.Mutex
 
 func main() {
-	atomic.StoreInt32(&connCount, 0)
+	//atomic.StoreInt32(&connCount, 0)
 
-	fmt.Println(atomic.LoadInt32(&connCount))
+	//fmt.Println(atomic.LoadInt32(&connCount))
 	n := 0
 	go func() {
 
 		for {
 			n++
-			//atomic.StoreInt32(&connCount, 0)
-			atomic.AddInt32(&connCount, 1)
-			//atomic.AddInt32(&connCount, -1)
-			//atomic.LoadInt32(&connCount)
+			mu.Lock()
+			connCount1++
+			mu.Unlock()
 			//fmt.Println(atomic.LoadInt32(&connCount))
 			//time.Sleep(time.Microsecond * 5)
 		}
@@ -37,5 +38,5 @@ func main() {
 
 	time.Sleep(time.Second*1)
 	fmt.Println(n)
-	fmt.Println(atomic.LoadInt32(&connCount))
+	fmt.Println(connCount1)
 }
